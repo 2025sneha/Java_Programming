3ff4ed5d427f64fa999bc8381e52aab2c398b1ab
Sneha Mothabhau Mohane <sneha.mohane1604@gmail.com>
2025-12-17 10:44:44 +0530

    Add files via upload

---
+++ ThisDemo.java
@@ -0,0 +1,37 @@
+class Demo
+{
+    public int i;
+    public static  int j;
+
+    static 
+    {
+        j = 21;
+    }
+
+    public Demo()
+    {
+        System.out.println("Inside default constructor");
+        this.i = 11;          // first use
+       
+    }
+
+    public Demo(int a)
+    {
+        this();           // second use
+        System.out.println("Inside parameterized constructor");
+    }
+
+    public void Display()
+    {
+        System.out.println("Inside Display"+this.i);          // third use
+    }
+}
+
+class ThisDemo
+{
+    public static void main(String A[] )
+    {
+        Demo dobj = new Demo(51);
+        dobj.Display();
+    }
+}
