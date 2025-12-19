05da10421ac41688e0e7ca172df331eab0b21f71
Sneha Mothabhau Mohane <sneha.mohane1604@gmail.com>
2025-12-19 20:07:46 +0530

    Add files via upload

---
+++ StaticBlock.java
@@ -0,0 +1,45 @@
+class Demo
+{
+    public int i;                // instance variable 
+    static public int j;     // class variable
+
+    static                      // static block to initialise static data
+    {
+        j = 21;
+    }
+
+    public Demo()
+    {
+        this.i = 11;        
+    }
+
+    public void fun()             // instance method
+    {
+        System.out.println("fun i : "+this.i);
+        System.out.println("fun j : "+Demo.j);
+    }
+
+    public static void gun()             // class method
+    {
+        // System.out.println("gun i : "+this.i);            // not allowed
+        System.out.println("gun j : "+Demo.j);
+    }
+}
+
+class StaticBlock
+
+{
+    public static void main(String A[])
+    {
+        Demo.gun();
+
+        System.out.println(Demo.j);
+
+        Demo dobj = new Demo();
+
+        System.out.println(dobj.i);
+        dobj.fun();
+        
+
+    }
+}
