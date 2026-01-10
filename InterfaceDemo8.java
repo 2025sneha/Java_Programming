e20ba89b72b5f407470036c4f192360ef405d040
Sneha Mothabhau Mohane <sneha.mohane1604@gmail.com>
2026-01-10 12:56:00 +0530

    Add files via upload

---
+++ InterfaceDemo8.java
@@ -0,0 +1,29 @@
+interface A
+{
+    int no = 11;
+    void fun();
+    default void gun()
+    {
+        System.out.println("Inside gun");
+    }
+}
+
+
+class Demo implements A,B   
+{
+    public void fun()
+    {
+        System.out.println("Inside fun ");    
+    }
+}
+
+class InterfaceDemo8
+{
+    public static void main(String A[])
+    {
+        Demo dobj = new Demo();
+
+        dobj.fun();
+        dobj.gun();
+    }
+}
