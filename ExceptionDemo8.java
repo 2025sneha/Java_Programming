399f83a1ea9703015e6ba0d66be597d37fa59c31
Sneha Mothabhau Mohane <sneha.mohane1604@gmail.com>
2026-01-18 14:07:27 +0530

    Add files via upload

---
+++ ExceptionDemo8.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class ExceptionDemo8
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iNo1 = 0, iNo2 = 0, iAns = 0;
+
+        System.out.println("Enter first number : ");
+        iNo1 = sobj.nextInt();
+
+        System.out.println("Enter Second number : ");
+        iNo2 = sobj.nextInt();
+        
+        try
+        {
+            System.out.println("Inside try block ");
+            iAns = iNo1 / iNo2;
+        }
+        
+        catch(ArithmeticException aobj)          // speicifc catch
+        {
+            System.out.println("Inside catch block ");  
+            System.out.println(aobj);
+        }
+        
+        catch(Exception eobj)         // Generic catch
+        {
+            System.out.println("Inside  generic catch block ");  
+        }
+
+        finally
+        {
+            System.out.println("Inside finally block");
+        }
+     
+        System.out.println("Division is : "+iAns);
+
+    }
+}
